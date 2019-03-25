11afb0d388a64c279b3102c7a394982c7554b413
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-03-25 14:28:21 +0100

    :sparkles: add petabc work

---
+++ pages/works/petabc/README.md
@@ -0,0 +1,4 @@
+# Semaine_Intensive_Front-End_P2022
+# GITHUB : https://github.com/ThPoi/Semaine_Intensive_Front-End_P2022/tree/master
+# WEBSITE : https://thpoi.github.io/Semaine_Intensive_Front-End_P2022/
+# AUTHORS : RICHARD Thibault, POIRIER Thomas, FERTE Ulysse, COMPAN Carla, MERCE Paul 
---
+++ pages/works/petabc/scripts/productPage.js
@@ -0,0 +1,86 @@
+
+// logo z index for mobile
+
+/////////////////////////////////////////////
+// Debut changement des objets///////////////
+/////////////////////////////////////////////
+
+document.querySelector(".models__containerBorder--red").addEventListener('click' , colorRed);
+
+document.querySelector(".models__containerBorder--red").addEventListener('click' , colorRed);
+
+
+document.querySelector(".models__containerBorder--black").addEventListener('click' , colorBlack);
+
+
+document.querySelector(".models__containerBorder--white").addEventListener('click' , colorWhite);
+
+let imgMachine = document.querySelector("img.models__img")
+let colorBackTop = document.querySelector(".colorBack--top")
+let colorBackBottom = document.querySelector(".colorBack--bottom")
+
+function colorRed(){
+  console.log("changement de couleur")
+  imgMachine.src = "../images/productPage/distributeurRed.png"
+  imgMachine.alt = "Distributeur automatique de croquette rouge"
+  colorBackTop.style.backgroundColor = "#DF424E"
+  colorBackBottom.style.backgroundColor = "#FDF51C"
+}
+
+function colorBlack(){
+  console.log("changement de couleur")
+  imgMachine.src = "../images/productPage/distributeurBlack.png"
+  imgMachine.alt = "Distributeur automatique de croquette noir"
+  colorBackTop.style.backgroundColor = "#434343"
+  colorBackBottom.style.backgroundColor = "#FBF4E2"
+}
+
+function colorWhite(){
+  console.log("changement de couleur")
+  imgMachine.src = "../images/productPage/distributeurWhite.png"
+  imgMachine.alt = "Distributeur automatique de croquette blanc"
+  colorBackTop.style.backgroundColor = "#FBF4E2"
+  colorBackBottom.style.backgroundColor = "#FDF51C"
+}
+/////////////////////////////////////////////
+// Fin changement des objets///////////////
+/////////////////////////////////////////////
+
+
+//////////////////////////////
+// Debut FAQ /////////////////
+//////////////////////////////
+var acc = document.getElementsByClassName("faq__div__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+/////////////////////////////////////////////
+// Fin FAQ ////////////////////////////////
+/////////////////////////////////////////////
+
+///////test
+
+let allQuestions = document.querySelectorAll(".faq__container article h4")
+
+for (let i = 0; i <allQuestions.length; i++){
+  allQuestions[i].addEventListener('click',function() {
+    if(this.parentNode.classList.contains('current')){
+      this.parentNode.classList.remove('current')
+    }
+    else{
+      let current = document.querySelector('.current')
+      this.parentNode.classList.add('current')
+    }
+  }
+)
+}
