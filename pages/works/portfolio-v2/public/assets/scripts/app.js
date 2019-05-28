bdb1e3120351d0976beae197d0574bd937f5ed7b
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-05-28 17:03:07 +0200

    :sparkles: add cursor model

--- pages/works/portfolio-v2/public/assets/scripts/app.js
+++ pages/works/portfolio-v2/public/assets/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('hello la mif')
+class Cursor
+{
+  constructor()
+  {
+    this.$cursor = document.querySelector('.js-cursor')
+    this.$cursor_in = document.querySelector('.js-cursor--in')
+  }
+  // Made marker img follow the mouse 
+  followCursor() {
+    let x = this.event.pageX
+    let y = this.event.pageY
+    this.$imgCursor.style.left = ((x + 1) - 25) + 'px'
+    this.$imgCursor.style.top = ((y + 1) - 70) + 'px'
+  }
+}
+
+
+
--- pages/works/portfolio-v2/views/pages/home.php
+++ pages/works/portfolio-v2/views/pages/home.php
@@ -1,7 +1,12 @@
 <?php require('../views/partials/header.php'); ?>
 
   <main class="main">
-    <h1 class="title">Hey ! I'm Thibault, french front-end developer</h1>
+    <h1 class="title">
+      Hey ! I'm Thibault, french front-end developer
+    </h1>
+    <div class="cursor js-cursor">
+      <div class="cursor--in js-cursor--in"></div>
+    </div>
   </main>
 
 <?php require('../views/partials/footer.php'); ?>
