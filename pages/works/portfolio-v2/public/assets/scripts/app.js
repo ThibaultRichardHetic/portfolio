83ebd2d320f656000ede51ecca257d0fd1db0162
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-06-01 17:41:12 +0200

    :sparkles: cursor custom follow mouse done

--- pages/works/portfolio-v2/public/assets/scripts/app.js
+++ pages/works/portfolio-v2/public/assets/scripts/app.js
@@ -1,18 +1,37 @@
+// Class for custom cursor
 class Cursor
 {
   constructor()
   {
     this.$cursor = document.querySelector('.js-cursor')
     this.$cursor_in = document.querySelector('.js-cursor--in')
+
+    // Get mouse position
+    window.addEventListener('mousemove', (e) =>
+    {
+      this.cursorX = e.clientX
+      this.cursorY = e.clientY
+      this.followCursorIn()
+      this.followCursorOut()
+    })
   }
+
   // Made marker img follow the mouse 
-  followCursor() {
-    let x = this.event.pageX
-    let y = this.event.pageY
-    this.$imgCursor.style.left = ((x + 1) - 25) + 'px'
-    this.$imgCursor.style.top = ((y + 1) - 70) + 'px'
+  followCursorIn() {
+    this.$cursor_in.style.left = (this.cursorX + 14) + 'px' 
+    this.$cursor_in.style.top = (this.cursorY + 14) + 'px' 
   }
+
+  followCursorOut() {
+    this.$cursor.style.left = this.cursorX + 'px' 
+    this.$cursor.style.top = this.cursorY + 'px' 
+  }
+
 }
 
+const cursor = new Cursor()
+
+window.onload = function () {
+}
 
 
--- pages/works/portfolio-v2/views/pages/home.php
+++ pages/works/portfolio-v2/views/pages/home.php
@@ -4,9 +4,8 @@
     <h1 class="title">
       Hey ! I'm Thibault, french front-end developer
     </h1>
-    <div class="cursor js-cursor">
-      <div class="cursor--in js-cursor--in"></div>
-    </div>
+    <div class="cursor js-cursor"></div>
+    <div class="cursor--in js-cursor--in"></div>
   </main>
 
 <?php require('../views/partials/footer.php'); ?>
